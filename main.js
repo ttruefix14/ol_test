97a5a1653dd19feb951f0f28838836bf41773200
ttruefix14 <95591262+ttruefix14@users.noreply.github.com>
2023-08-16 14:49:27 +0300

    Update main.js

--- main.js
+++ main.js
@@ -9,20 +9,20 @@ const layers = [
     source: new OSM(),
   }),
   new TileLayer({
-    extent: [4417220.367986957, 5536949.394112153, 4423805.018839356, 5561616.5029378375],
+    extent: [4397126.97247314, 5454955.65368652, 4433964.04187012, 5499907.26367188],
     source: new TileWMS({
-      url: 'http://192.168.0.23:8080/geoserver/ows?version=1.3.0',
-      params: {'LAYERS': 'admenp', 'TILED': true},
+      url: 'http://ec2-34-239-125-112.compute-1.amazonaws.com:8080/geoserver/ows?version=1.3.0',
+      params: {'LAYERS': 'ne:np_view', 'TILED': true},
       serverType: 'geoserver',
       // Countries have transparency, so do not fade tiles:
       transition: 0,
     }),
   }),
   new TileLayer({
-    extent: [4409873, 5533087.5, 4432068, 5566840.5],
+    extent: [4397126.97247314, 5454955.65368652, 4433964.04187012, 5499907.26367188],
     source: new TileWMS({
-      url: 'http://192.168.0.23:8080/geoserver/ows?version=1.3.0',
-      params: {'LAYERS': 'functionalzone', 'TILED': true},
+      url: 'http://ec2-34-239-125-112.compute-1.amazonaws.com:8080/geoserver/ows?version=1.3.0',
+      params: {'LAYERS': 'ne:fz_view', 'TILED': true},
       serverType: 'geoserver',
       // Countries have transparency, so do not fade tiles:
       transition: 0,
@@ -33,7 +33,7 @@ const map = new Map({
   layers: layers,
   target: 'map',
   view: new View({
-    center: [4420000, 5550000],
+    center: [4410000, 5470000],
     zoom: 12,
   }),
 });
