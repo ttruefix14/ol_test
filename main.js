6725e6b7dd4ca3f1a8e8625fdb61bfb327cd5157
ttruefix14 <95591262+ttruefix14@users.noreply.github.com>
2023-08-17 17:46:19 +0300

    Update main.js

--- main.js
+++ main.js
@@ -8,24 +8,26 @@ const layers = [
   new TileLayer({
     source: new OSM(),
   }),
-  new TileLayer({
-    extent: [4397126, 5454955, 4433964, 5499907],
-    source: new TileWMS({
-      url: 'http://10.10.5.50:8080/geoserver/ows?service=WMS&version=1.3.0',
-      params: {'LAYERS': 'ne:fz_view', 'TILED': true},
-      serverType: 'geoserver',
-      // Countries have transparency, so do not fade tiles:
-      transition: 0,
+  new LayerGroup({
+    new TileLayer({
+      extent: [4397126, 5454955, 4433964, 5499907],
+      source: new TileWMS({
+        url: 'http://10.10.5.50:8080/geoserver/ows?service=WMS&version=1.3.0',
+        params: {'LAYERS': 'ne:fz_view', 'TILED': true},
+        serverType: 'geoserver',
+        // Countries have transparency, so do not fade tiles:
+        transition: 0,
+      }),
     }),
-  }),
-  new TileLayer({
-    extent: [4397126, 5454955, 4433964, 5499907],
-    source: new TileWMS({
-      url: 'http://10.10.5.50:8080/geoserver/ows?service=WMS&version=1.3.0',
-      params: {'LAYERS': 'ne:np_view', 'TILED': true},
-      serverType: 'geoserver',
-      // Countries have transparency, so do not fade tiles:
-      transition: 0,
+    new TileLayer({
+      extent: [4397126, 5454955, 4433964, 5499907],
+      source: new TileWMS({
+        url: 'http://10.10.5.50:8080/geoserver/ows?service=WMS&version=1.3.0',
+        params: {'LAYERS': 'ne:np_view', 'TILED': true},
+        serverType: 'geoserver',
+        // Countries have transparency, so do not fade tiles:
+        transition: 0,
+      }),
     }),
   }),
 ];
@@ -36,10 +38,34 @@ const map = new Map({
     center: [4410000, 5470000],
     zoom: 11,
   }),
-function layersOnOff(toggle){
-    var LayerList = map.layers;
-    for(var a = 0; a < LayerList.length; a++ ){
-        LayerList[a].visibility = toggle;
-    };
-};
 });
+function bindInputs(layerid, layer) {
+  const visibilityInput = $(layerid + ' input.visible');
+  visibilityInput.on('change', function () {
+    layer.setVisible(this.checked);
+  });
+  visibilityInput.prop('checked', layer.getVisible());
+
+  const opacityInput = $(layerid + ' input.opacity');
+  opacityInput.on('input', function () {
+    layer.setOpacity(parseFloat(this.value));
+  });
+  opacityInput.val(String(layer.getOpacity()));
+}
+function setup(id, group) {
+  group.getLayers().forEach(function (layer, i) {
+    const layerid = id + i;
+    bindInputs(layerid, layer);
+    if (layer instanceof LayerGroup) {
+      setup(layerid, layer);
+    }
+  });
+}
+setup('#layer', map.getLayerGroup());
+
+$('#layertree li > span')
+  .click(function () {
+    $(this).siblings('fieldset').toggle();
+  })
+  .siblings('fieldset')
+  .hide();
