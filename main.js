78b4a91d5ba4fa136443dd71a6018dea9131e88f
ttruefix14 <95591262+ttruefix14@users.noreply.github.com>
2023-08-17 17:10:11 +0300

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const layers = [
   new TileLayer({
     extent: [4397126.97247314, 5454955.65368652, 4433964.04187012, 5499907.26367188],
     source: new TileWMS({
-      url: '10.10.5.50:8080/geoserver/ows?service=WMS&version=1.3.0',
+      url: 'http://10.10.5.50:8080/geoserver/ows?service=WMS&version=1.3.0',
       params: {'LAYERS': 'ne:fz_view', 'TILED': true},
       serverType: 'geoserver',
       // Countries have transparency, so do not fade tiles:
@@ -21,7 +21,7 @@ const layers = [
   new TileLayer({
     extent: [4397126.97247314, 5454955.65368652, 4433964.04187012, 5499907.26367188],
     source: new TileWMS({
-      url: '10.10.5.50:8080/geoserver/ows?service=WMS&version=1.3.0',
+      url: 'http://10.10.5.50:8080/geoserver/ows?service=WMS&version=1.3.0',
       params: {'LAYERS': 'ne:np_view', 'TILED': true},
       serverType: 'geoserver',
       // Countries have transparency, so do not fade tiles:
